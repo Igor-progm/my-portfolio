694c8bd47fea00fb2732e7ee88f04c4a8da4236b
Igor-progm <igor.antunes39@hotmail.com>
2025-03-11 23:45:52 -0300

    melhorando responsividade em telas a partir de 500px

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ document.querySelectorAll('.nav-menu a').forEach(link => {
         navMenu.classList.remove('active');
     })
 })
+
+function revelarAoRolar() {
+    const elementoProjetos = document.querySelector('.meus-projetos-container');
+    const elementoHabilidades = document.querySelector('.habilidades-container');
+    const alturaDaTela = window.innerHeight;
+
+    if (elementoProjetos) {
+        const posicaoProjetos = elementoProjetos.getBoundingClientRect().top;
+        if (posicaoProjetos < alturaDaTela - 100) {
+            elementoProjetos.classList.add('aparecer');
+        }
+    }
+
+    if (elementoHabilidades) {
+        const posicaoHabilidades = elementoHabilidades.getBoundingClientRect().top;
+        if (posicaoHabilidades < alturaDaTela - 100) {
+            elementoHabilidades.classList.add('aparecer');
+        }
+    }
+}
+
+window.addEventListener('scroll', revelarAoRolar);
